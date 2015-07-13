4620e764e3d36d6ebd2c19dac6206d18845ce2ea
Administrator <samksan@live.com>
2015-07-14 06:30:01 +0800

    网络数据获取
    在 MainActivity 中 实现 “获取数据”按钮的第一部分
    连接网络并获取数据

--- app/src/main/java/com/example/administrator/myfive/MainActivity.java
+++ app/src/main/java/com/example/administrator/myfive/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.myfive;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,9 +8,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class MainActivity extends ActionBarActivity {
+    private TextView tv;
+
     private Spinner s1,s2,s3,s4,s5;
     Integer[] dataSource = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
@@ -57,7 +70,7 @@ public class MainActivity extends ActionBarActivity {
 
     public void getData(View view) {
         //从网络获取开奖数据并存入数据库
-
+        readURL("http://chart.cp.360.cn/zst/p3?span=30");
 
 
 
@@ -78,5 +91,75 @@ public class MainActivity extends ActionBarActivity {
 
 
 
+    }
+
+    /**
+     * 从网络读取数据
+     * @param url 网址
+     */
+    public void readURL(String url){
+        new AsyncTask<String,Void,String>(){
+
+            @Override
+            protected String doInBackground(String... params) {
+                try {
+                    URL url = new URL(params[0]);
+
+                    URLConnection urlConnection = url.openConnection();
+                    urlConnection.setConnectTimeout(1000 * 10);
+
+                    InputStream is = urlConnection.getInputStream();
+
+                    InputStreamReader isr = new InputStreamReader(is,"GBK");
+                    BufferedReader br = new BufferedReader(isr);
+
+                    String line;
+                    StringBuilder builder = new StringBuilder();
+
+                    while ((line = br.readLine()) != null) {
+                        builder.append(line);
+                    }
+
+                    br.close();
+                    isr.close();
+                    is.close();
+                    return builder.toString();
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                Toast.makeText(MainActivity.this, "开始连接网络更新数据", Toast.LENGTH_SHORT).show();
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                tv = (TextView) findViewById(R.id.tv);
+                tv.setText(s);
+                super.onPostExecute(s);
+            }
+
+            @Override
+            protected void onProgressUpdate(Void... values) {
+                super.onProgressUpdate(values);
+            }
+
+            @Override
+            protected void onCancelled(String s) {
+                super.onCancelled(s);
+            }
+
+            @Override
+            protected void onCancelled() {
+                super.onCancelled();
+            }
+        }.execute(url);
     }
 }
